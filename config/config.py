0e06e9e3c847b2af233e7d78597d710b98b5ada1
Masahme <115889084+Masahme@users.noreply.github.com>
2023-03-16 14:28:15 +0200

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -44,8 +44,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alexa_BotUpdates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Alexa_Help")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/source_av")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/JK_ZG")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
